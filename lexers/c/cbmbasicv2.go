cd08e7dacf7c36faba11a0fa5f401070fece8315
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2020-11-17 11:31:00 -0300

    Add CBM BASIC V2 lexer text analysis

--- lexers/c/cbmbasicv2.go
+++ lexers/c/cbmbasicv2.go
@@ -1,9 +1,13 @@
 package c
 
 import (
+	"regexp"
+
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
 )
+
+var cbmBasicV2AnalyserRe = regexp.MustCompile(`^\d+`)
 
 // CBM BASIC V2 lexer.
 var CbmBasicV2 = internal.Register(MustNewLexer(
@@ -16,4 +20,12 @@ var CbmBasicV2 = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	// if it starts with a line number, it shouldn't be a "modern" Basic
+	// like VB.net
+	if cbmBasicV2AnalyserRe.MatchString(text) {
+		return 0.2
+	}
+
+	return 0
+}))
